3544fc293f583bd471584fb923ad15cd68ab52f1
Shamyralieva Zeinep <zeinepshamyralieva01@gmail.com>
2024-10-19 16:54:09 +0600

    Created Navlink navbar layout, added Home page to Routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './containers/Home/Home.tsx';
+import './App.css';
+
 const App = () => {
   return (
-    <div>
-      <p>Hello world</p>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg main-nav">
+        <div className="container">
+          <NavLink className="navbar-brand" to="/">Soroso Godies</NavLink>
+          <div>
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/">Home</NavLink>
+              </li>
 
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/about">About Us</NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/contacts">Contacts</NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/portfolio">Portfolio</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="container mt-3">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </div>
     </div>
   );
 };
